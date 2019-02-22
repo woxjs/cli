cbbdd1b981d9fce345c077df1cadc6c208f6dbd6
evio <evio@vip.qq.com>
2019-02-22 16:42:09 +0800

    update 2.0.0

--- index.js
+++ index.js
@@ -1,85 +1,53 @@
-#!/usr/bin/env node
-
 const fs = require('fs');
 const path = require('path');
-const pkg = require('./package.json');
-const program = require('commander');
-const catcher = require('@clusic/catch');
-const utils = require('@clusic/utils');
-const { Signale } = require('signale');
-const Addone = require('./lib/addone');
+module.exports = (program, client) => {
+  program
+    .command('wox:new [project]')
+    .description('create a new wox project or plugin')
+    .option('-p, --plugin', 'create new plugin mode')
+    .action(client.require('./lib/create-new-project'));
 
-const cmd = new Signale({
-  interactive: true,
-  scope: 'wox'
-});
+  program
+    .command('wox <path>')
+    .description('create a new wox file by type')
+    .option('-p, --component [type]', 'create a new `Vue.component` file')
+    .option('-d, --directive', 'create a new `Vue.directive` file')
+    .option('-f, --filter', 'create a new `Vue.filter` file')
+    .option('-x, --mixin', 'create a new `Vue.mixin` file')
+    .option('-c, --controller', 'create a new `Controller` file')
+    .option('-m, --middleware', 'create a new `Middleware` file')
+    .option('-s, --service', 'create a new `Service` file')
+    .option('-t, --decorate', 'create a new `Decorate` file')
+    .option('-w, --webview', 'create a new `Decorate` file')
+    .action(client.require('./lib/add-new-file'));
 
-program.version(pkg.version, '-v, --version');
+  const plugins = [];
+  const rootConfigPath = path.resolve(process.cwd(), 'config/plugin.json');
+  const packageFilePath = path.resolve(process.cwd(), 'package.json');
 
-program
-  .command('new [project]')
-  .description('create a new project')
-  .action(project => tryCatch(async roll => await exec('new', roll, project)));
+  if (fs.existsSync(rootConfigPath)) {
+    plugins = Object.keys(require(rootConfigPath));
+  } else {
+    if (fs.existsSync(packageFilePath)) {
+      const pkg = require(packageFilePath);
+      if (pkg.plugin) {
+        plugins = pkg.plugin.dependencies;
+      }
+    }
+  }
 
-program
-  .command('add <files...>')
-  .description('add files by params')
-  .option('-C, --controller', 'add a controller file')
-  .option('-M, --middleware', 'add a middleware file')
-  .option('-S, --service', 'add a service file')
-  .option('-W, --webview', 'add a webview file')
-  .option('-P, --component', 'add a component file')
-  .option('-D, --directive', 'add a directive file')
-  .option('-F, --filter', 'add a filter file')
-  .option('-A, --async', 'the webview must be a async webview')
-  .allowUnknownOption()
-  .action((...args) => tryCatch(async roll => await exec('add', roll, ...args)));
-
-program
-  .command('install <plugins...>')
-  .alias('i')
-  .description('setup wox plugins')
-  .action(plugins => tryCatch(async roll => await exec('install', roll, ...plugins)));
-
-program
-  .command('uninstall <plugins...>')
-  .alias('d')
-  .description('remove wox plugins')
-  .action(plugins => tryCatch(async roll => await exec('uninstall', roll, ...plugins)));
-
-program
-  .command('use <commander>')
-  .description('use npm or custom registry commander')
-  .action(commander => tryCatch(async roll => await exec('use', roll, commander)));
-
-const pluginFilePath = path.resolve(process.cwd(), 'plugin', 'index.json');
-if (fs.existsSync(pluginFilePath)) {
-  const plugins = utils.loadFile(pluginFilePath);
-  for (const plugin in plugins) {
-    if (!plugins[plugin].enable) continue;
-    const filePath = path.resolve(process.cwd(), 'node_modules', plugin, '.command.js');
-    if (fs.existsSync(filePath)) {
-      const fileExports = utils.loadFile(filePath);
-      if (typeof fileExports === 'function') {
-        fileExports(program, {
-          tryCatch, exec, Addone
-        });
+  for (let i = 0; i < plugins.length; i++) {
+    const plugin = plugins[i];
+    let modulePath;
+    try{ modulePath = require.resolve(plugin); }catch(e) {}
+    if (modulePath) {
+      const moduleExports = require(modulePath);
+      const moduleDir = path.dirname(modulePath);
+      if (typeof moduleExports === 'function' && moduleExports.__IS_CLI_PLUGIN__) {
+        moduleExports(program, new client.constructor(moduleDir, ctx.util, ctx.pkg));
       }
     }
   }
 }
 
-program.parse(process.argv);
-
-function tryCatch(callback) {
-  return catcher(callback, e => cmd.error(e.message));
-}
-
-async function exec(name, ...args) {
-  if (typeof name === 'function') {
-    return await name(cmd);
-  }
-  const res = utils.loadFile(path.resolve(__dirname, 'commander', name + '.js'));
-  if (typeof res !== 'function') throw new Error('can not find the commander of ' + name);
-  return await res(cmd, ...args);
-}
+module.exports.__IS_CLI_PLUGIN__ = true;
